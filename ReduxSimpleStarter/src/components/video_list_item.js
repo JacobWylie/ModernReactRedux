cc540c979afee85787a7d22198dd499760415d60
JacobWylie <jacobwylie@gmail.com>
2017-08-25 18:10:03 +0200

    load videos to page with style

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = props => {
-	return <li>Video</li>
+// es6 syntax: {video} takes the param and declares a new variable with same name
+const VideoListItem = ({video}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+	console.log(video)
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<h4>{video.snippet.title}</h4>
+					<h6>By: {video.snippet.channelTitle}</h6>
+					<p>{video.snippet.description}</p>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default VideoListItem;
