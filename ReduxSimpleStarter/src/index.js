1f32bf39975b9a3b6917dc1bf3508baf4fbb4541
JacobWylie <jacobwylie@gmail.com>
2017-09-14 17:11:24 +0200

    redux

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,51 +1,15 @@
-import _ 					from 'lodash';
-import React, { Component } from 'react';
-import ReactDOM 			from 'react-dom';
-import YTSearch 			from 'youtube-api-search';
-import SearchBar 			from './components/search_bar';
-import VideoList 			from './components/video_list';
-import VideoDetail 			from './components/video_detail';
-// const API_KEY = process.env.API_KEY;
-const API_KEY = 'AIzaSyB2tVLl0U8zOhG3FBgO60MQPRkdfb3mbU4';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-		this.state = { 
-			videos: [],
-			selectedVideo: null
-		};
-
-		this.videoSearch('javascript');
-	}
-
-	videoSearch(term) {
-		// Make an API call to search YouTube
-		YTSearch({key: API_KEY, term: term}, videos => {
-			//es6 same key:value name typed as one word ie. {videos; videos} 
-			this.setState({ 
-				videos: videos,
-				selectedVideo: videos[0]
-			 })
-		});
-	}
-
-	render() {
-		const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
-
-		return (
-			<div>
-				<SearchBar onSearchTermChange={videoSearch} />
-				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList  
-					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-					videos={this.state.videos} />
-			</div>
-		);
-	}
-}
-
-//Take this component's generated HTML and put it in the DOM
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Learn to Code' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Narnia' },
+		{ title: 'Harry Potter' }
+	]
+}
