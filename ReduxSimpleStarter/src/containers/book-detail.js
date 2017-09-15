9723be5f472ea011838662061e3e6dc3cb086d42
JacobWylie <jacobwylie@gmail.com>
2017-09-15 14:42:29 +0200

    render book detail to page with conditions

--- ReduxSimpleStarter/src/containers/book-detail.js
+++ ReduxSimpleStarter/src/containers/book-detail.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class BookDeatil extends Component {
+class BookDetail extends Component {
 	render() {
+		if(!this.props.book) {
+			return <div>Select a book to get started.</div>
+		}
+
 		return (
-			<div>Book Detail</div>
+			<div className="col-sm-8">
+				<h3>Details for book:</h3>
+				<div><h5>Title:</h5> {this.props.book.title}</div>
+				<div><h5>Pages:</h5> {this.props.book.pages}</div>
+				<div><h5>Synopsis:</h5> {this.props.book.synopsis}</div> 
+			</div>
 		);
-	};
-};
+	}
+}
 
 function mapStateToProps(state) {
 	return {
 		book: state.activeBook
 	};
-};
+}
 
 export default connect(mapStateToProps)(BookDetail);
--- ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -1,8 +1,20 @@
 export default function() {
 	return [
-		{ title: 'Learn to Code' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Narnia' },
-		{ title: 'Harry Potter' }
+		{ title: 'Learn to Code', 
+		  pages: 101,
+		  synopsis: "Street art irony forage, salvia vape vinyl whatever fam. Deep v tacos narwhal letterpress shabby chic hexagon. Cloud bread echo park artisan waistcoat hella forage normcore twee tattooed. Adaptogen pitchfork whatever bushwick, yr drinking vinegar tofu gastropub mustache next level vape cardigan."
+		},
+		{ title: 'The Dark Tower', 
+		  pages: 85,
+		  synopsis: "Whatever lyft freegan selvage four dollar toast lomo flexitarian. Lo-fi narwhal vexillologist franzen kickstarter banjo. Etsy vexillologist roof party williamsburg, la croix +1 ethical. Heirloom vaporware pok pok, activated charcoal flannel ethical direct trade celiac brooklyn ugh hella stumptown cornhole la croix. Af small batch chia migas you probably haven't heard of them. " 
+		},
+		{ title: 'Narnia', 
+		  pages: 250, 
+		  synopsis: "Lomo actually flannel, raw denim vinyl edison bulb raclette shabby chic pour-over microdosing shoreditch bitters. Ethical cronut gastropub, lumbersexual paleo deep v small batch keytar prism flannel offal enamel pin DIY. Meditation biodiesel celiac, affogato actually disrupt selvage."
+		},
+		{ title: 'Harry Potter', 
+		  pages: 812,
+		  synopsis: "Activated charcoal bespoke chicharrones hot chicken shaman la croix lyft polaroid hexagon hoodie. Echo park fingerstache chambray, gochujang ennui mumblecore truffaut next level artisan meditation wolf cardigan VHS tousled. Farm-to-table tumblr chambray, heirloom flexitarian food truck sustainable glossier kickstarter beard keytar banh mi pabst."
+		}
 	]
 }
