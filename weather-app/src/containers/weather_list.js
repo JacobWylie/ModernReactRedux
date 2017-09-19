5cb26358343e7d23c3ea3c03cc28798a89b27b4b
JacobWylie <jacobwylie@gmail.com>
2017-09-19 13:52:22 +0200

    style weather graphs. Link to weather site by city id

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const country = cityData.city.country;
 		const temps = cityData.list.map(weather => weather.main.temp);
-		// const pressure = cityData.list.map(weather => weather.main.pressure);
-		// const humidity = cityData.list.map(weather => weather.main.humidity);
+		const wind = cityData.list.map(weather => weather.wind.speed);
+		const humidity = cityData.list.map(weather => weather.main.humidity);
+		const link = `https://openweathermap.org/city/${cityData.city.id}`;
 
 		return(
 			<tr key={name}>
-				<td>{name}, {country}</td>
 				<td>
-					<Sparklines height={120} with={100} data={temps}>
-						<SparklinesLine color='red' />
+					<a href={link} target='_blank'> 
+						{'  '}{name}, {country}
+					</a>
+				</td>
+				<td>
+					<Sparklines height={100} width={180} data={temps}>
+						<SparklinesLine color='red' style={{ fill: "grey" }}/>
+						<SparklinesReferenceLine type="avg" />
 					</Sparklines>
 				</td>
-				<td>{}</td>
-				<td>{}</td>
+				<td>
+					<Sparklines height={120} width={180} data={wind}>
+						<SparklinesLine color='blue' style={{ fill: "grey" }}/>
+						<SparklinesReferenceLine type="avg" />
+					</Sparklines>
+				</td>
+				<td>
+					<Sparklines height={120} width={180} data={humidity}>
+						<SparklinesLine color='green' style={{ fill: "grey" }}/>
+						<SparklinesReferenceLine type="avg" />
+					</Sparklines>
+				</td>
 			</tr>
 		)
 	}
@@ -30,9 +46,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (℃)</th>
+						<th>Wind (km/h)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
