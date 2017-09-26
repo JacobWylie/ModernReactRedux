ac02783a66a9f4ff461f6a5b211faf1435103494
JacobWylie <jacobwylie@gmail.com>
2017-09-26 17:02:11 +0200

    form validation styling and conditional error messages

--- blog-app/src/components/posts_new.js
+++ blog-app/src/components/posts_new.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 	renderField(field) {
+		const { meta: { touched, error } } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				<div className="text-help">
+					{touched ? error: ''}
+				</div>
 			</div>
 		);
 	}
 
+	onSubmit(values) {
+		console.log(values)
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
+
+
 		return (
 			<div>
 				<h3>Add a New Blog Post</h3>
-				<form>
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 					<Field
 						label="Title of Post"
 						name="title"
@@ -26,7 +40,7 @@ class PostsNew extends Component {
 					/>
 					<Field
 						label="Categories"
-						name="Categories"
+						name="categories"
 						component={this.renderField}
 					/>
 					<Field
@@ -34,6 +48,9 @@ class PostsNew extends Component {
 						name="content"
 						component={this.renderField}
 					/>
+
+					<button type="submit" className="btn btn-sm btn-primary">Submit</button>
+					<Link to='/' className='btn btn-sm btn-danger'>Cancel</Link>
 				</form>
 			</div>
 		);
@@ -53,7 +70,7 @@ function validate(values) {
 	}
 
 	if(!values.content) {
-		errors.content = "Enter some content please";
+		errors.content = "Enter some content";
 	}
 
 	// If errors is empty, the form is fine to submit
