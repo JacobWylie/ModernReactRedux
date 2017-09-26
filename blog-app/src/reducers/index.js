ba85e2703f627ee724f99ce7f00829aebcf56af7
JacobWylie <jacobwylie@gmail.com>
2017-09-26 14:49:46 +0200

    redux form added and for component initiated

--- blog-app/src/reducers/index.js
+++ blog-app/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import POSTSREDUCER from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: POSTSREDUCER
+  posts: POSTSREDUCER,
+  form: formReducer
 });
 
 export default rootReducer;
